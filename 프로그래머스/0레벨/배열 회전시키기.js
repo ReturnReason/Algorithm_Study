32ef51d230311fea537b0c0e60eeac7fd08a4788
ReturnReason <reasonz@daum.net>
2023-01-19 21:58:42 +0900

    프로그래머스 0레벨: 배열 회전시키기(JS)

---
+++ 프로그래머스/0레벨/배열 회전시키기.js
@@ -0,0 +1,8 @@
+function solution(numbers, direction) {
+  if (direction === 'right')
+    return [
+      numbers[numbers.length - 1],
+      ...numbers.slice(0, numbers.length - 1),
+    ];
+  else return [...numbers.slice(1), numbers[0]];
+}
