a11593cc5d83c004e5b79f19e6056948c0f37246
ReturnReason <reasonz@daum.net>
2023-03-12 03:26:36 +0900

    백준 9498: 시험 성적(JS)

---
+++ 백준/Bronze/9498_시험 성적.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const a = +inputs[0];
+
+if (a >= 90 && a <= 100) console.log('A');
+else if (a >= 80) console.log('B');
+else if (a >= 70) console.log('C');
+else if (a >= 60) console.log('D');
+else console.log('F');
