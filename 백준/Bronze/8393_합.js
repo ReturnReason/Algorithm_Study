dd5d5830c2fb63ee8ba3b3b85f5c15f9b10b52f3
ReturnReason <reasonz@daum.net>
2023-03-12 18:26:11 +0900

    백준 8393: 합 - 등차수열 풀이(JS)

--- 백준/Bronze/8393_합.js
+++ 백준/Bronze/8393_합.js
@@ -6,3 +6,12 @@ for (let i = 1; i <= inputs[0]; i++) {
   result += i;
 }
 console.log(result);
+
+/**
+ * 등차수열 풀이
+ * */
+// const fs = require('fs');
+// const inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(inputs[0]);
+console.log((n * (n + 1)) / 2);
